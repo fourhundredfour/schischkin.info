b1ac9dfa5e05aabb41143b4e42db800f74339c82
Daniel Schischkin <daniel@schischkin.info>
2020-01-21 23:52:15 +0100

    [WIP] Implement header

---
+++ src/components/Header.svelte
@@ -0,0 +1,27 @@
+<style lang="scss">
+    $output-color: rgba(255, 255, 255, .9);
+    .console-link {
+        color: $output-color;
+        transition: all .5s;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    header {
+        position: relative;
+        width: 100%;
+    }
+    h1 {
+        text-align: center;
+    }
+</style>
+<script>
+import Terminal from './Terminal.svelte';
+</script>
+<header>
+    <h1>Daniel Schischkin</h1>
+    <Terminal>
+        Apprenticeship as IT Specialist with focus on application development at <a target="_blank" class="console-link" href="https://surplex.com">Surplex GmbH</a>.
+    </Terminal>
+</header>
---
+++ src/components/Terminal.svelte
@@ -0,0 +1,54 @@
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Gothic+A1&display=swap');
+$input-color: rgb(0, 167, 58);
+$output-color: rgba(255, 255, 255, .9);
+.terminal {
+    font-family: 'Gothic A1', sans-serif;
+    position: relative;
+    background: rgba(61, 61, 61, 1);
+    padding: 10px;
+    min-height: 2rem;
+    border-radius: 2px;
+    box-shadow: 1px 1px 1px rgba(61, 61, 61, .5);
+    cursor: text;
+    .input {
+        background: none;
+        border: none;
+        color: $input-color;
+        padding-left: 1rem;
+        line-height: 2;
+        &:before {
+            content: '$ ';
+            font-weight: bold;
+        }
+    }
+    .output {
+        padding-left: 1rem;
+        color: $output-color;
+    }
+}
+</style>
+<script>
+    import Typewriter from 'svelte-typewriter';
+	import { fade } from 'svelte/transition';
+ 
+    let showOutput = false;
+    let cursorColor = "rgb(0, 167, 58)";
+    setTimeout(() => {
+        showOutput = true;
+        cursorColor = null;
+    }, 1200);
+</script>
+
+<div class="terminal">
+    <Typewriter cursor={cursorColor} interval={50}>
+        <span class="input">./current_job.sh</span>
+    </Typewriter>
+    {#if showOutput}
+        <span class="output" transition:fade="{{duration: 400}}">
+            <slot>
+                Hello world!
+            </slot>
+        </span>
+    {/if}
+</div>
